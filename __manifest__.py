1bb58f3ec91e52bd00dea6f5d2777221e18e6614
Sachitha Perera <sperera@nurosoft.id>
2022-06-20 09:48:14 +0530

    [ADD] Padukka ware house added

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'VA Distilleries',
+    'version': '1',
+    'website': 'https://www.subtletechs.com/',
+    'author': 'Subtle Technologies (Pvt) Ltd',
+    'depends': [
+        'base',
+        'stock',
+        'purchase',
+    ],
+    'data': [
+        'data/warehouses.xml'
+    ],
+
+    'installable': True,
+    'application': True,
+    'auto_install': True
+}
