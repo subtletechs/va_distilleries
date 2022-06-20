627fb471c09c4a4e2f6532e0f17f0e95071ee847
Milan <milanb@subtletechs.com>
2022-06-20 23:45:50 +0530

    [UPDATED]- [VA|IMP|-002] - Update Inventory Locations

--- stock/stock_move.py
+++ stock/stock_move.py
@@ -7,6 +7,14 @@ from odoo.exceptions import UserError, ValidationError
 from collections import defaultdict
 
 
+class Location(models.Model):
+    _inherit = "stock.location"
+
+    # [VA|IMP|-002] - Update Inventory Locations
+    mobile_location = fields.Boolean(string="Mobile Location", default=False)
+    main_location = fields.Boolean(string="Main Location", default=False)
+
+
 class Warehouse(models.Model):
     _inherit = "stock.warehouse"
 
