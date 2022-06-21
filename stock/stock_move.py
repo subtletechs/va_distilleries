5849ae4a2d2a00db1266c51e5cd5bfaaae775a87
Milan <milanb@subtletechs.com>
2022-06-21 13:18:43 +0530

    [UPDATED]- [VA|IMP|-004] - Add new approval level to all operations in stock picking

--- stock/stock_move.py
+++ stock/stock_move.py
@@ -7,6 +7,18 @@ from odoo.exceptions import UserError, ValidationError
 from collections import defaultdict
 
 
+class Picking(models.Model):
+    _inherit = "stock.picking"
+
+    # [VA|IMP|-004]- Add new approval level to all operations
+    state = fields.Selection(selection_add=[('approved', 'Approved'), ('done',)])
+
+    def action_approve(self):
+        self.write({
+            'state': 'approved'
+        })
+
+
 class Location(models.Model):
     _inherit = "stock.location"
 
