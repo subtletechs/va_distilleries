24dda389e906f6cf77d5ae125a130ef8cb919b8b
Sachitha Perera <sperera@nurosoft.id>
2022-06-23 04:31:21 +0530

    [UPDATED] Negative stock transfer  -  logic updated for locations

--- stock/stock_picking.py
+++ stock/stock_picking.py
@@ -15,8 +15,8 @@ class StockPicking(models.Model):
 
     def button_validate(self):
         """In here We will check negative quantities and block it for specific inventory operations"""
-        # 1. Delivery Orders
-        if self.picking_type_code == 'outgoing':
+        # Delivery orders/  Internal Transfers
+        if self.picking_type_code in ['outgoing', 'internal']:
             stock_moves = self.move_ids_without_package
             for stock_move in stock_moves:
                 product_id = stock_move.product_id
@@ -24,15 +24,46 @@ class StockPicking(models.Model):
                 # check tracking method
                 tracking_method = product_id.tracking
                 if tracking_method in ['lot', 'serial']:
-                    move_lines = stock_move.move_line_ids
-                    for move_line in move_lines:
-                        lot = move_line.lot_id
-                        qty_done = move_line.qty_done
-                        if qty_done > lot.product_qty:
-                            error = "Lot "+lot.name+" Don't have enough quantities"
-                            raise ValidationError(error)
+                    if stock_move.move_line_ids:
+                        move_lines = stock_move.move_line_ids
+                        for move_line in move_lines:
+                            location_id = move_line.location_id
+                            lot = move_line.lot_id
+                            qty_done = move_line.qty_done
+                            # check the availability of quantity
+                            stock_quant = self.env['stock.quant'].search(
+                                [('location_id', '=', location_id.id),
+                                 ('product_id', '=', product_id.id),
+                                 ('lot_id', '=', lot.id)])
+                            if stock_quant:
+                                quantity = stock_quant.quantity
+                                if quantity < qty_done:
+                                    error = "Product " + str(product_id.name) + ", Lot " + str(
+                                        lot.name) + " Dose not have enough quantities at " + str(location_id.name)
+                                    raise ValidationError(error)
+                            else:
+                                error = "Product " + str(product_id.name) + ", Lot " + str(
+                                    lot.name) + " Dose not have enough quantities at " + str(location_id.name)
+                                raise ValidationError(error)
+
                 elif tracking_method == 'none':
-                    if done_qty > product_id.qty_available:
-                        error = product_id.name+ "Doesn't have enough quantity"
-                        raise ValidationError(error)
+                    if stock_move.move_line_ids:
+                        move_lines = stock_move.move_line_ids
+                        for move_line in move_lines:
+                            location_id = move_line.location_id
+                            qty_done = move_line.qty_done
+                            stock_quant = self.env['stock.quant'].search(
+                                [('location_id', '=', location_id.id),
+                                 ('product_id', '=', product_id.id)])
+                            if stock_quant:
+                                quantity = stock_quant.quantity
+                                if quantity < qty_done:
+                                    error = "Product " + str(
+                                        product_id.name) + " Dose not have enough quantities at " + str(
+                                        location_id.name)
+                                    raise ValidationError(error)
+                            else:
+                                error = "Product " + str(
+                                    product_id.name) + " Dose not have enough quantities at " + str(location_id.name)
+                                raise ValidationError(error)
         return super(StockPicking, self).button_validate()
